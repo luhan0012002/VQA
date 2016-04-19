b1b8eb4e7567ca37e7d886fcda2b4bc4eb091fc8
luhan <luhan@luhans-MacBook-Pro.local>
2016-04-19 00:25:11 -0400

    unify indentation

--- generateAns.lua
+++ generateAns.lua
@@ -30,7 +30,7 @@ function GenerateAns.generateAns(ds_test, protos, ans_path)
     local candidates = {}
     local answers = {}
     for n = 1, nBatches_test do
-	print(string.format("%d/%d", n, nBatches_test))
+        print(string.format("%d/%d", n, nBatches_test))
         -- get next training/testing batch
         local words, fc7, conv4, targets = torch.LongTensor(),torch.LongTensor(),torch.LongTensor(), torch.LongTensor() 
         words, fc7, conv4, targets = words:cuda(), fc7:cuda(), conv4:cuda(), targets:cuda()
@@ -41,22 +41,22 @@ function GenerateAns.generateAns(ds_test, protos, ans_path)
         fc7 = inputs[2]
         conv4 = inputs[3]
         local local_batchSize = words:size(1)
-	if local_batchSize < batchSize then
-		assert(n == nBatches_test)
-		words = torch.cat(words, torch.CudaTensor(batchSize-local_batchSize, rho):fill(0), 1)
-		fc7 = torch.cat(fc7, torch.CudaTensor(batchSize-local_batchSize, fcSize):fill(0), 1)
-		conv4 = torch.cat(conv4, torch.CudaTensor(batchSize-local_batchSize, 196, 512):fill(0), 1)
-		targets = torch.cat(targets, torch.CudaTensor(batchSize-local_batchSize):fill(0), 1)
-	end
+        if local_batchSize < batchSize then
+            assert(n == nBatches_test)
+            words = torch.cat(words, torch.CudaTensor(batchSize-local_batchSize, rho):fill(0), 1)
+            fc7 = torch.cat(fc7, torch.CudaTensor(batchSize-local_batchSize, fcSize):fill(0), 1)
+            conv4 = torch.cat(conv4, torch.CudaTensor(batchSize-local_batchSize, 196, 512):fill(0), 1)
+            targets = torch.cat(targets, torch.CudaTensor(batchSize-local_batchSize):fill(0), 1)
+        end
         local totalInput = {fc7, words, conv4}
         -- forward step
-	local batchSize = words:size(1)
+        local batchSize = words:size(1)
         local totalOutput, err = Train.foward(clones, protos, totalInput, targets, batchSize)
-	print(err)
+        print(err)
 
         local outputs = totalOutput[6]
         local nQuestions = math.min(batchSize/4, local_batchSize/4 )
-	local start_idx = (n-1) * batchSize+1
+        local start_idx = (n-1) * batchSize+1
         local end_idx = n * batchSize
         if end_idx > ds_test.size then
                 end_idx = ds_test.size
